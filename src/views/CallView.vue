0ee0345e84109499aecf108d9301c087a350ed7e
Fábio de Lima <fabiolima@github.com>
2025-03-03 16:33:58 -0300

    Add avatar

--- src/views/CallView.vue
+++ src/views/CallView.vue
@@ -3,6 +3,7 @@
   {{ myPeer?.id }}
   <button @click="copyJoinCallCommand">Compartilhar meu peerId {{ myPeer?.id }}</button>
   <button @click="callPeer(route.query.peerId)" v-if="route.query.peerId">Call Peer</button>
+  <Avatar />
   <div v-if="!localStream">
     <p>Clique no botão abaixo para iniciar a chamada.</p>
     <button @click="startCall">Iniciar Chamada</button>
@@ -29,6 +30,7 @@ import { ref, onMounted, nextTick } from 'vue'
 import { useRoute } from 'vue-router'
 import { usePeerConnection } from '@/composables/usePeerConnection'
 import { useMediaDevices } from '@/composables/useMediaDevices'
+import Avatar from '@/components/Avatar.vue'
 
 const { myPeer, startPeerConnection, answerCallsWith, call } = usePeerConnection()
 const { getUserMedia } = useMediaDevices()
@@ -38,6 +40,45 @@ const remoteVideo = ref(null)
 const route = useRoute()
 
 const localStream = ref(null)
+
+const setupAudioAnalyser = async (stream) => {
+  try {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+    const analyser = audioContext.createAnalyser()
+    const microphone = audioContext.createMediaStreamSource(stream)
+    microphone.connect(analyser)
+    analyser.fftSize = 256
+
+    const bufferLength = analyser.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+
+    function animateWaves() {
+      analyser.getByteFrequencyData(dataArray)
+
+      // Calcula o volume médio do áudio
+      const averageVolume = dataArray.reduce((sum, value) => sum + value, 0) / bufferLength
+
+      // Se o volume for alto o suficiente, cria uma onda
+      if (averageVolume > 50) {
+        console.log('volume alto')
+        // const wave = document.createElement('div')
+        // wave.classList.add('wave')
+        // waveContainer.appendChild(wave)
+        //
+        // // Remove a onda após a animação
+        // setTimeout(() => {
+        //   wave.remove()
+        // }, 500)
+      }
+
+      requestAnimationFrame(animateWaves)
+    }
+
+    animateWaves()
+  } catch (error) {
+    console.error('Erro ao acessar o microfone:', error)
+  }
+}
 
 const copyJoinCallCommand = () => {
   const baseUrl = import.meta.env.DEV
@@ -49,6 +90,8 @@ const copyJoinCallCommand = () => {
 
 const startMediaStream = async () => {
   const stream = await getUserMedia()
+
+  if (stream.getAudioTracks().length) await setupAudioAnalyser(stream)
 
   if (stream) {
     localStream.value = stream
